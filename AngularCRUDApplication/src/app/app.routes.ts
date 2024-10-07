279ca9f9174a2a061c11b1ad0b947a02edd7d8ec
Ziyaad <ziyaad.parkerct@gmail.com>
2024-10-07 11:03:54 +0200

    Added more files

--- AngularCRUDApplication/src/app/app.routes.ts
+++ AngularCRUDApplication/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'Home', pathMatch: 'full'},
+    {path: 'Home', component: HomeComponent},
+    {path: 'ViewEmployee/:employeeId', component: ViewEmployeeComponent},
+    {path: 'AddEmployee', component: AddEmployeeComponent},
+    {path: 'EditEmployee/:employeeId', component: EditEmployeeComponent}
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
